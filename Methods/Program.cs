23cbd370a82340549535788646377b536f76a184
Yao Xiao <Yao Xiao@DESKTOP-AT0LLHN>
2019-01-30 17:43:03 -0500

    Here is my HW

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+    Author: Yao Xiao    
+    Date: 1/30/2019
+    Comments: This C# Console application code demonstrates the use of
+              methods after getting input from a user name.
+*/
+using System;
 
 namespace Methods
 {
@@ -6,7 +12,10 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello," + name);
+            Console.ReadLine();
         }
     }
 }
